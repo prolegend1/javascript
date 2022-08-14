fcf29f55814cce717d40775ea50aaad48e7f1568
Hilal Ahmad <prolegend8822@gmail.com>
2022-08-14 17:51:15 +0500

    this is the first version

---
+++ bootcamp/pages/js1.js
@@ -0,0 +1,41 @@
+console.log('hello sir');
+
+// string 
+let city ='Peshawar'
+let country = 'Pakistan'
+let locationt = city + ', ' + country
+console.log(locationt)
+
+//numbers
+let studentscore= 18
+let maxscore=20
+let percentage=(studentscore/maxscore)*100
+console.log('your percentage is '+percentage)
+//variables
+let petName= 'Cat'
+console.log(petName)
+ petName= 'Dog'
+console.log(petName)
+
+// convert fahrenheit to celcius
+let fahrenheit=68
+let celcius=(68-32)/(9/5)
+console.log(celcius)
+//boolean operator
+// === equality 
+// !== No equal 
+// tem <23  
+// <=  
+
+let temp ='yes'
+let raining = temp !=='yes'
+console.log('Today is raining '+raining)
+ 
+// chellange area
+let age=7
+let childe= age <=7
+console.log("you got discount because you are childe "+childe)
+
+let older= age >=34
+console.log("you got discount because you are older "+older)
+
